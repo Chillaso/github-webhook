92bbac2374c42291a4a0f0d89f2771302b9839cb
Chillaso <carlos.chillagonzalez@gmail.com>
2021-03-23 13:52:19 +0100

    Add endpoint and use express json parser

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 "use strict";
 
 const { Octokit } = require("@octokit/core");
+const express = require('express');
+const app = express()
+const port = 8081
+const octokit = new Octokit({auth: `${process.env.AUTH_TOKEN}`});
 
-async function main() {
-const octokit = new Octokit({auth: `787b7faf634784d01ce27c19d52482442a87b771`});
+app.use(express.json());
 
-const response = await octokit.request("POST /repos/gg-restock/pcc-checker/deployments", {
-    payload: {test: "test"}
-})
+app.post('/checker/deploy', async (req, res) => {
 
+    process.on('unhandledRejection', _ => {});
+    console.log(req.body);
+    let status = await requestDeployment(req.body.URL_ITEM, req.body.ID_ITEM, req.body.MAX_PRICE);
+    res.sendStatus(status);
+
+});
+
+async function requestDeployment(url, id, price) {
+    try{
+        var response = await octokit.request("POST /repos/gg-restock/pcc-checker/deployments", {
+            headers: {
+                accept: "application/vnd.github.v3+json"
+            },
+            ref: "main",
+            payload: {  
+                URL_ITEM: url,
+                ID_ITEM: id,
+                MAX_PRICE: price
+            }
+        });
+    } catch(error){
+        console.log(error);
+    }
+
+    if(response.status == 201){
+        console.log(`Deployed checker with url: ${url} --- id: ${id} and max price: ${price}`);
+    } else {
+        console.log('Failed to deploy checker');
+    }
+    return response.status;
 }
-main();
+
+app.listen(port, () => {
+    console.log(`Server up, listening on ${port}`)
+})
