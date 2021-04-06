3a6c2226cd7a6fda4a148716a306f56db11611be
Chillaso <carlos.chillagonzalez@gmail.com>
2021-04-06 20:43:10 +0200

    Add deployname to the log

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ app.use(express.json());
 app.post('/checker/deploy', async (req, res) => {
 
     process.on('unhandledRejection', _ => {});
-    let status = await requestDeployment(req.body.URL_ITEM, req.body.ID_ITEM, req.body.MAX_PRICE);
+    let status = await requestDeployment(req.body);
     res.sendStatus(status);
 
 });
 
-async function requestDeployment(url, id, price) {
+async function requestDeployment(body) {
     let url = body.URL_ITEM
     let id = body.ID_ITEM
     let price = body.MAX_PRICE
@@ -34,7 +34,7 @@ async function requestDeployment(url, id, price) {
             DEPLOY_NAME: deployName
         }
     })
-    .then(response => console.log(`${response.status} - Deployment created for product: ${url} with id: ${id} and max price: ${price}`))
+    .then(response => console.log(`${response.status} - Deployment ${deployName} created for product: ${url} with id: ${id} and max price: ${price}`))
     .catch(error => console.log(error));
 }
 
